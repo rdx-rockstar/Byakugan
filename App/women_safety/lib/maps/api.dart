10a2335d9d4aa71609051ba9696b9dc780930ebe
Mohd Sahil <mohdsahil032@gmail.com>
2021-04-10 14:51:29 +0530

    Map Page Added

---
+++ App/women_safety/lib/maps/api.dart
@@ -0,0 +1,5 @@
+class Secrets {
+  // Add your Google Maps API Key here
+  static const API_KEY = 'AIzaSyDlLOCHDhMFhbjKV-R3w4GLzNmVW62rG-U';
+  // static const API_KEY = 'AIzaSyClsvmZLFTmqyPigpbdxS-N_gShBKpFFKw';
+}
